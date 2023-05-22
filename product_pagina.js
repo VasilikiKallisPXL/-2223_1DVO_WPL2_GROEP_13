61033e2ed6864f771fc13bb6382da941633aa448
AnaYacariniPXL <ana.YacariniGonzales@student.pxl.be>
2023-05-22 23:40:30 +0200

    product_pagina-itemsProduct

--- product_pagina.js
+++ product_pagina.js
@@ -1,12 +1,15 @@
-let winkelmandje = document.querySelectorAll(".btn-item_toevoegen");
 
+
+let kopen = document.querySelectorAll(".btn-item_toevoegen");
 const  productenLijstParts = document.querySelector("#producten_container_parts");
 const btnWinkelmandje = document.querySelector('.container-mandje-icoon');
 const containerMandjeProduct= document.querySelector('.container-cart-products');
 
+
 btnWinkelmandje.addEventListener('click',() =>{
     containerMandjeProduct.classList.toggle('hidden-mandje')
 })
+
 
 const productenParts = [
     {
@@ -90,28 +93,110 @@ const productenParts = [
     }
 ];
 
+
+
+/////////////Render werkt niet met de teller/////////
+
+
+// function productenRenderen(){
+//
+//     productenParts.forEach((product) => {
+//         const cardsParts = document.createElement('div');
+//         cardsParts.classList.add('card_parts', 'col-md-4', 'col-lg-3')
+//
+//         cardsParts.innerHTML = ` <div class="card card-product-onderdelen__">
+//                 <div class="card text-bg-dark">
+//                     <img src="${product.afbeelding}" alt="foto_card" class="img-fluid"/>
+//                     <a href="product_detail_station.html">
+//
+//                     </a>
+//                 </div>
+//
+//                 <div class="card-img-overlay klein_card">
+//                     <button type="button" class="btn btn-link-secondary border border-0 btn_card_klein btn-item_toevoegen"
+//                             data-bs-toggle="modal" data-bs-target="#product_toegevoegd " id=" ${product.id}"  ><a
+//                             class="nav-link"
+//                             style="color: white"
+//                             href="#"><i
+//                             class="bi bi-cart2 fs-5"></i></a></button>
+//                     <button type="button" class="btn btn-link-secondary float-end border border-0 btn_card_klein"
+//                             data-bs-toggle="modal"
+//                             data-bs-target="#favoriet_toegevoegd2"><a
+//                             class="nav-link" style="color: white" href="#"><i class="bi bi-heart fs-5 "></i></a>
+//                     </button>
+//
+//
+//                 </div>
+//                 <div class="card-body" style="background-color: rgba(11,7,45,0.86)" >
+//                 <h5 class="card-title float-start" style="color: #5EBCAF">${product.naam}</h5>
+//                     <p class="card-text float-start" style="color:#ffffff">${product.beschrijving}</p>
+//                     <p class="fs-6 fw-bold  float-end prijs" style="color:#ffffff">€ ${product.prijs}</p>
+//
+//                 </div>
+//
+//             </div>
+//  `;
+//
+//
+//         productenLijstParts.appendChild(cardsParts);
+//
+//     })
+// }
+// productenRenderen();
+
+
+document.addEventListener('ContentLoaded', iTemsToevoegenWinkelmandje);
+
+function iTemsToevoegenWinkelmandje(){
+    const winkelmandje = JSON.parse(localStorage.getItem('winkelmandje'));
+    productenLijstParts.addEventListener('click', (e) => {
+        if (e.target.classList.contains('btn-item_toevoegen')) {
+            const product = e.target.parentElement;
+
+            if (winkelmandje) {
+                winkelmandje.forEach((product) => {
+                    const {id, naam, prijs} = product;
+                    productToevoegen(id, naam, prijs);
+                });
+            }
+
+            }
+
+            for(let i=0; i<kopen.length; i++){
+                const { id, naam, prijs } = productenParts[i];
+                kopen[i].addEventListener('click', ()=>{
+                    aantalItemsMandje(productenParts[i]);
+                    totalCost(productenParts[i]);
+                    productToevoegen(id, naam, prijs);
+                });
+
+        }
+
+    })
+
+}
+
+function productToevoegen(id, naam, prijs){
+    const winkelmandje=JSON.parse(localStorage.getItem('winkelmandje'))||[]
+
+    const product ={
+        id, naam , prijs
+    };
+
+    winkelmandje.push(product);
+    localStorage.setItem('winkelmandje',JSON.stringify(winkelmandje) )
+}
+
+
+
+/////////////////
 const productenLijstJson= JSON.stringify(productenParts);
 localStorage.setItem('productenParts', productenLijstJson);
 
-// prueba
-
-if(typeof (Storage)!=='undefined'){
-
-}
+//
 
 
 
-
-
-// fin prueba
-
-for(let i=0; i<winkelmandje.length; i++){
-    winkelmandje[i].addEventListener('click', ()=>{
-        aantalItemsMandje(productenParts[i]);
-
-        totalCost(productenParts[i]);
-    })
-}
 function aantalItemsMandje(productParts){
 
     let productNummmers = localStorage.getItem('aantalItemsMandje')
@@ -140,64 +225,40 @@ function totalCost(productParts){
 
 }
 
+const productZichtbaarWinkelmandje = document.createElement('div');
+productZichtbaarWinkelmandje.classList.add('product_zichtbaar_winkelmandje');
+
+productZichtbaarWinkelmandje.innerHTML =  `
+ 
+                                        <div class="cart-product">
+                                            <div class="info-cart-product">
+                                                <span class="aantal_items">1</span>
+                                                <p class="naam_product">${naam}</p>
+                                                <span class="prijs_product">$80</span>
+                                            </div>
+                                            <i class="bi bi-x fs-2"></i>
+                                        </div>
+                                
+                                    <div class="cart-total">
+                                        <h3>Total:</h3>
+                                        <span class="total-cost">200</span>
+                                    </div>
+
+`
+
+
+
+
 function onLoadAantalItemsMandje(){
+
      let productNummers = localStorage.getItem('AantalItemsMandje');
     document.querySelector('.producten_opteller').textContent = productNummers;
 
 }
+iTemsToevoegenWinkelmandje()
+productToevoegen()
 onLoadAantalItemsMandje()
 
-
-
-
-
-function productenRenderen(){
-
-    productenParts.forEach((info) => {
-        const cardsParts = document.createElement('div');
-        cardsParts.classList.add('card_parts', 'col-md-4', 'col-lg-3')
-
-        cardsParts.innerHTML = ` <div class="card card-product-onderdelen__">
-                <div class="card text-bg-dark">
-                    <img src="${info.afbeelding}" alt="foto_card" class="img-fluid"/>
-                    <a href="product_detail_station.html">
-                      
-                    </a>
-                </div>
-
-                <div class="card-img-overlay klein_card">
-                    <button type="button" class="btn btn-link-secondary border border-0 btn_card_klein btn-item_toevoegen"
-                            data-bs-toggle="modal" data-bs-target="#product_toegevoegd " id=" ${info.id}"  ><a
-                            class="nav-link"
-                            style="color: white"
-                            href="#"><i
-                            class="bi bi-cart2 fs-5"></i></a></button>
-                    <button type="button" class="btn btn-link-secondary float-end border border-0 btn_card_klein"
-                            data-bs-toggle="modal"
-                            data-bs-target="#favoriet_toegevoegd2"><a
-                            class="nav-link" style="color: white" href="#"><i class="bi bi-heart fs-5 "></i></a>
-                    </button>
-
-
-                </div>
-                <div class="card-body" style="background-color: rgba(11,7,45,0.86)" >
-                <h5 class="card-title float-start" style="color: #5EBCAF">${info.naam}</h5>
-                    <p class="card-text float-start" style="color:#ffffff">${info.beschrijving}</p>
-                    <p class="fs-6 fw-bold  float-end prijs" style="color:#ffffff">€ ${info.prijs}</p>
-
-                </div>
-
-            </div>
- `;
-
-
-        productenLijstParts.appendChild(cardsParts);
-
-
-    })
-}
-
-// const containerCartProduct = document.querySelector('#producten_container_parts')
 
 
 
